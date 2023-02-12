5309d80e7c30614cbb8de90b6d2a508d785bf8b4
João Fuzeto <fuzeto.jf@gmail.com>
2023-02-12 20:38:45 +0000

    Creating and adding Clinic Project API

---
+++ api_clinic/src/main/java/br/com/clinic/api_clinic/ApiClinicApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinic.api_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicApplication.class, args);
+	}
+
+}
---
+++ api_clinic/src/test/java/br/com/clinic/api_clinic/ApiClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinic.api_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
